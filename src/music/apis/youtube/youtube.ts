410ad08c42cbb820c2cf30936d0757c58477a3b0
Hector Escolante Enriquez <inforeshector@gmail.com>
2024-04-29 16:11:27 +0200

    FEAT: query

--- src/music/apis/youtube/youtube.ts
+++ src/music/apis/youtube/youtube.ts
@@ -1,6 +1,7 @@
 import MusicProviders from '../../providers/music-providers';
 import ytdl from 'ytdl-core';
 import { ErrorHandler } from '../../../shared/error.handler';
+import YoutubeUrlFactory from './url-factory';
 
 export enum SearchElements {
     query = 'query',
@@ -9,26 +10,35 @@ export enum SearchElements {
 
 export default class YoutubeHandler {
     private musicProviders: MusicProviders
+    private validUrl!: string
+
     constructor () {
         this.musicProviders = new MusicProviders()
     }
 
     async search(query: string) {
         try {
-            // URL CASES
-            const youtube = this.musicProviders.musicProviders.youtube.youtube;
-            const searchString = /^(http(s)?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w- ;,./?%&=]*)?$/i.test(query) ? query : `ytsearch:${query}`;
+            const youtube = this.musicProviders.musicProviders.youtube.youtube
+            const searchString = /^(http(s)?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w- ;,./?%&=]*)?$/i.test(query) ? query : `ytsearch:${query}`
+            const isURL = /^(http(s)?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w- ;,./?%&=]*)?$/i.test(searchString)
 
-            const videos = await youtube.searchVideos(searchString, 1);
+            const videos = await youtube.searchVideos(searchString, 1)
 
+            console.log('IS URL', isURL)
+            console.log('SEARCH STRING', searchString)
+            console.log('VIDEOS', videos)
+            
             if (!videos) {
-                //TOD@ no resultados en youtube por URL
-                //TOD@ search for string
-                new ErrorHandler('🎬 ', 'YOUTUBE-HANDLER', 'No results found for url search')
+                new ErrorHandler('🎬 ', 'YOUTUBE-HANDLER', `No results found for ${query}`)
                 return null
             }
+            if (!isURL) {
+                const urlFactory = new YoutubeUrlFactory(videos[0].id)
+                this.validUrl= urlFactory.getUrl()
+            }
 
-            const basicInfo = await ytdl.getBasicInfo(searchString)
+
+            const basicInfo = await ytdl.getBasicInfo((isURL) ? searchString : this.validUrl)
 
             const song = {
                 found_by: SearchElements.url,
@@ -36,7 +46,7 @@ export default class YoutubeHandler {
                 title: basicInfo.videoDetails.title,
                 thumbnail: basicInfo.videoDetails.thumbnail,
                 duration: basicInfo.videoDetails.lengthSeconds,
-            };
+            }
 
             return song;
         
